977d12a0072f60100156c0661114d5da4158a95c
hsj <hsj_sukey@163.com>
2020-07-08 23:28:57 +0800

    上传了vue demo

---
+++ hsj/src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="hello">
+    <el-button type="text" @click="hello">helloworld</el-button>
+    helloworld
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  methods: {
+    hello: function () {
+      console.log("lhr")
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
