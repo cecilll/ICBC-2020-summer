b57a55bb3be13bc3a50a1ea5f8c8852514ccae64
KyrieLee0823 <39258878+KyrieLee0823@users.noreply.github.com>
2020-07-10 09:44:33 +0800

    Spring Security+Jwt Login /unfinished

---
+++ lhg/src/main/java/com/icbc/orient/OrientApplication.java
@@ -0,0 +1,14 @@
+package com.icbc.orient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrientApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(OrientApplication.class, args);
+    }
+
+}
