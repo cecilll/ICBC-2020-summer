925d56f65a451ef68fe4787ac78bb2c5c44d4208
KyrieLee0823 <39258878+KyrieLee0823@users.noreply.github.com>
2020-07-15 22:42:04 +0800

    登录接口

---
+++ orient-app/src/main/java/com/icbc/orient/Bean/ReturnType.java
@@ -0,0 +1,23 @@
+package com.icbc.orient.Bean;
+
+public class ReturnType {
+
+            String code;
+            String msg;
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+}
